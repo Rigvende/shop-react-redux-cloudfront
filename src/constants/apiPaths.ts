a31741fee20859c4870d06b220d030d125c3cf08
Marianna Patrusova <sumeragi.tanako@gmail.com>
2021-08-29 20:12:07 +0300

    cleanup-2

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev/products',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
