12f90d57e485938ecb0cda105a432d2fc3b32369
Marianna Patrusova <sumeragi.tanako@gmail.com>
2021-09-22 21:00:56 +0300

    change API path for import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://0b7450r3x2.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://uqswt6bp2h.execute-api.eu-west-1.amazonaws.com/dev',
 };
