2c1a7371e0e023640280332778d56d9d7772fa41
Marianna Patrusova <sumeragi.tanako@gmail.com>
2021-10-17 18:29:47 +0300

    add error response statuses

--- src/index.tsx
+++ src/index.tsx
@@ -13,8 +13,14 @@ axios.interceptors.response.use(
     return response;
   },
   function(error) {
-    if (error?.response?.status === 400) {
+    const responseStatus = error?.response?.status;
+
+    if (responseStatus === 400) {
       alert(error.response.data?.data);
+    }
+
+    if (responseStatus === 401 || responseStatus === 403) {
+      alert(error.response.data?.message);
     }
 
     return Promise.reject(error?.response ?? error);
@@ -31,7 +37,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
