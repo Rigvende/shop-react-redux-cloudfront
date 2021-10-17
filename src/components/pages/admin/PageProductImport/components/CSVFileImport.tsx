421dcaa4c72ae257cb908f49d6945bbe7b00a0ea
Marianna Patrusova <sumeragi.tanako@gmail.com>
2021-10-17 19:48:43 +0300

    use local storage for token

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -30,17 +30,19 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
   };
 
   const uploadFile = async (e: any) => {
-      // Get the presigned URL
+      localStorage.setItem('authorization_token', 'Basic 1faOfsalfj343fcz0fddF4r==')
+
       const response = await axios({
         method: 'GET',
         url,
         params: {
           name: encodeURIComponent(file.name)
         },
-        headers: { Authorization: 'Basic 1faOfsalfj343fcz0fddF4r==' }
+        headers: { Authorization: localStorage.getItem('authorization_token') }
       })
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
+
       const result = await fetch(response.data, {
         method: 'PUT',
         body: file
