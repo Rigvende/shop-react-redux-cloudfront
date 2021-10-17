84351a70cc252bd58eaca3f2286fe481f0e0ca36
Marianna Patrusova <sumeragi.tanako@gmail.com>
2021-10-17 19:39:35 +0300

    add authorization

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -36,7 +36,8 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
         url,
         params: {
           name: encodeURIComponent(file.name)
-        }
+        },
+        headers: { Authorization: 'Basic 1faOfsalfj343fcz0fddF4r==' }
       })
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
